58d2055b2e046d69833523050f951955ca66a053
Daniel Barreto <danielfsbarreto@gmail.com>
2025-10-06 19:49:30 -0300

    Configure streamable-http server

--- src/crewai_expert/main.py
+++ src/crewai_expert/main.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
 from crewai.flow import Flow, listen, router, start
+from mcp.server.fastmcp import Context
 
 from src.crewai_expert.crews import AnswerCrewaiPromptCrew
 from src.crewai_expert.services import DocFilesChunkingService
@@ -7,50 +7,46 @@ from src.crewai_expert.types import CrewaiExpertState
 
 
 class CrewaiExpertFlow(Flow[CrewaiExpertState]):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
+    def __init__(self, context: Context, *args, **kwargs):
+        self.context = context
         self.doc_files_chunking_service = DocFilesChunkingService()
 
+        super().__init__(*args, **kwargs)
+
     @start()
-    def validate_inputs(self):
+    async def validate_inputs(self):
+        await self.context.report_progress(
+            progress=0, total=3, message="Validating inputs..."
+        )
         if self.state.run_type == "answer_prompt" and self.state.prompt is None:
             raise ValueError("The input `prompt` needs to be provided")
 
     @router(validate_inputs)
-    def identify_path(self):
+    async def identify_path(self):
+        await self.context.report_progress(
+            progress=1, total=3, message="Identifying path..."
+        )
         return self.state.run_type
 
     @listen("update_embeddings")
     async def update_embeddings_path(self):
+        await self.context.report_progress(
+            progress=2, total=3, message="Updating embeddings..."
+        )
         await self.doc_files_chunking_service.call()
 
     @listen("answer_prompt")
-    def come_up_with_curated_answer(self):
-        result = (
+    async def come_up_with_curated_answer(self):
+        await self.context.report_progress(
+            progress=2, total=3, message="Coming up with a curated answer..."
+        )
+        result = await (
             AnswerCrewaiPromptCrew(
                 collection_name=self.doc_files_chunking_service.collection_name
             )
             .crew()
-            .kickoff(inputs={"prompt": self.state.prompt})
+            .kickoff_async(inputs={"prompt": self.state.prompt})
         )
         self.state.final_answer = result.raw
 
         return {"final_answer": self.state.final_answer}
-
-
-def kickoff():
-    # CrewaiExpertFlow().kickoff(inputs={"run_type": "update_embeddings"})
-    CrewaiExpertFlow().kickoff(
-        inputs={
-            "prompt": "Me relate todas as formas de criar uma tool customizada com CrewAI"
-        }
-    )
-
-
-def plot():
-    CrewaiExpertFlow().plot()
-
-
-if __name__ == "__main__":
-    kickoff()
